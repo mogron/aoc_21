28eb57952d5a1696aebb694be80ef9eeb5de0abb
Moritz Gronbach <meismo@gmail.com>
2021-12-15 21:37:38 +0100

    update d14

--- 14/soln.py
+++ 14/soln.py
@@ -1,17 +1,9 @@
 from collections import Counter, defaultdict
 
 
-def main():
-    with open("input.txt") as f:
-        s, _, *inp = f.read().splitlines()
-    rules = {}
-    for rule in inp:
-        a, b = rule.split(" -> ")
-        a1, a2 = a
-        rules[(a1, a2)] = b
+def solve(s, rules, iterations):
     pair_counts = defaultdict(lambda: 0, Counter(zip(s, s[1:])))
     letter_counts = defaultdict(lambda: 0, Counter(s))
-    iterations = 40
     for _ in range(iterations):
         for pair, cnt in list(pair_counts.items()):
             if pair in rules:
@@ -22,6 +14,18 @@ def main():
                 pair_counts[pair] -= cnt
                 letter_counts[middle] += cnt
     print(max(letter_counts.values()) - min(letter_counts.values()))
+
+
+def main():
+    with open("input.txt") as f:
+        s, _, *inp = f.read().splitlines()
+    rules = {}
+    for rule in inp:
+        a, b = rule.split(" -> ")
+        a1, a2 = a
+        rules[(a1, a2)] = b
+    solve(s, rules, 10)
+    solve(s, rules, 40)
 
 
 if __name__ == "__main__":
