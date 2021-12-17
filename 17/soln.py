6dbab30a333b4f573a5b9fa79bec7e8f39dc13e9
Moritz Gronbach <meismo@gmail.com>
2021-12-17 18:39:28 +0100

    add day 17

---
+++ 17/soln.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+
+def solve():
+    pass
+
+
+def main():
+    x_min, x_max = 48, 70
+    y_min, y_max = -189, -148
+
+    y_pos_max = 0
+    total = 0
+    for initial_y_vel in range(y_min - 1, -y_min + 1):
+        for initial_x_vel in range(1, 71):
+            x = 0
+            y = 0
+            y_pos_max_traj = 0
+            x_vel = initial_x_vel
+            y_vel = initial_y_vel
+            while y >= y_min:
+                x += x_vel
+                y += y_vel
+                y_pos_max_traj = max(y_pos_max_traj, y)
+                y_vel -= 1
+                x_vel = max(0, x_vel - 1)
+                if x_min <= x <= x_max and y_min <= y <= y_max:
+                    total += 1
+                    y_pos_max = max(y_pos_max, y_pos_max_traj)
+                    break
+    print(y_pos_max)
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
