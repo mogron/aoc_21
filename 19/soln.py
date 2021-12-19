63fe2eea35c9f28dbe6778d717988b7c1ac7bc8e
Moritz Gronbach <meismo@gmail.com>
2021-12-19 15:19:12 +0100

    add type hints

--- 19/soln.py
+++ 19/soln.py
@@ -1,6 +1,9 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 from math import sin, cos, pi
 from itertools import product, combinations
+from typing import Callable
 
 
 @dataclass
@@ -9,26 +12,31 @@ class Point:
     y: int
     z: int
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(self.x) + hash(self.y) + hash(self.z)
 
 
-def subtract(a: Point, b: Point):
+Scanner = set[Point]
+RotationFunction = Callable[[Point], Point]
+
+
+def subtract(a: Point, b: Point) -> Point:
     return Point(a.x - b.x, a.y - b.y, a.z - b.z)
 
 
-def add(a: Point, b: Point):
+def add(a: Point, b: Point) -> Point:
     return Point(a.x + b.x, a.y + b.y, a.z + b.z)
 
 
-def l1(a: Point, b: Point):
+def l1(a: Point, b: Point) -> int:
     """Compute L1/Manhattan distance between a and b"""
     return abs(a.x - b.x) + abs(a.y - b.y) + abs(a.z - b.z)
 
 
-def rotation_fun(a, b, c):
-    # a, b, c = rotation angles
-    # matrix from here: https://de.wikipedia.org/wiki/Roll-Nick-Gier-Winkel
+def rotation_fun(a: float, b: float, c: float) -> RotationFunction:
+    """Returns a function that rotates a point by the specified rotation angles a, b, c.
+    Rotation matrix taken from here: https://de.wikipedia.org/wiki/Roll-Nick-Gier-Winkel
+    """
     ca = cos(a)
     cb = cos(b)
     cc = cos(c)
@@ -45,7 +53,7 @@ def rotation_fun(a, b, c):
     m32 = cb * sc
     m33 = cb * cc
 
-    def rotate(p):
+    def rotate(p: Point) -> Point:
         x = p.x * m11 + p.y * m12 + p.z * m13
         y = p.x * m21 + p.y * m22 + p.z * m23
         z = p.x * m31 + p.y * m32 + p.z * m33
@@ -54,7 +62,7 @@ def rotation_fun(a, b, c):
     return rotate
 
 
-def make_rotation_functions():
+def make_rotation_functions() -> list[RotationFunction]:
     u = pi / 2
     rotations = []
     for a_rot in range(4):
@@ -77,42 +85,28 @@ def make_rotation_functions():
     return reduced_rotations
 
 
-def parse_scanner(inp):
-    beacons = []
-    # skip first line
-    try:
-        next(inp)
-    except StopIteration:
-        return None
-    for line in inp:
-        if line == "":
-            break
-
-    return beacons
-
-
-def parse_scanners(inp):
+def parse_scanners(inp: list[str]) -> list[Scanner]:
     scanners = []
-    beacons = set()
+    scanner: Scanner = set()
     for line in inp:
         if line == "" or line.startswith("---"):
-            if beacons:
-                scanners.append(beacons)
-                beacons = set()
+            if scanner:
+                scanners.append(scanner)
+                scanner = set()
             continue
         x, y, z = map(int, line.split(","))
         p = Point(x, y, z)
-        beacons.add(p)
-    if beacons:
-        scanners.append(beacons)
+        scanner.add(p)
+    if scanner:
+        scanners.append(scanner)
     return scanners
 
 
-def compute_pairwise_distances(scanner):
+def compute_pairwise_distances(scanner: Scanner) -> set[int]:
     return set([l1(a, b) for a, b in combinations(scanner, 2)])
 
 
-def maybe_match(scanner_a, scanner_b):
+def is_match_possible(scanner_a: Scanner, scanner_b: Scanner) -> bool:
     """By comparing intra-scanner pairwise distances, check if there is any chance that scanner_a and scanner_b match."""
     dists_a = compute_pairwise_distances(scanner_a)
     dists_b = compute_pairwise_distances(scanner_b)
@@ -124,55 +118,53 @@ def maybe_match(scanner_a, scanner_b):
     return n_common >= n_required
 
 
-def find_next_match(q, scanners, rotations):
+def find_next_match(q, scanners, rotations) -> tuple[int, Scanner, Point]:
     # find next scanner that matches one of the solved scanners
     for scanner_a in q:
         for scanner_b_idx, scanner_b in enumerate(scanners):
-            if not maybe_match(scanner_a, scanner_b):
+            if not is_match_possible(scanner_a, scanner_b):
                 continue
             # try all rotations
             for rotation in rotations:
                 scanner_b_rotated = [rotation(beacon) for beacon in scanner_b]
-                # how many matches if the i-th beacon of scanner_a was the j-th beacon of scanner_b
                 for beacon_a, beacon_b in product(scanner_a, scanner_b_rotated):
+                    # count matches if beacon_a was the same as beacon_b
                     dist = subtract(beacon_a, beacon_b)
                     t_beacons = set([add(b, dist) for b in scanner_b_rotated])
                     inter = t_beacons.intersection(scanner_a)
                     if len(inter) >= 12:
                         # found a match
                         return scanner_b_idx, t_beacons, dist
-    # no match shouldn't happen, given the problem specification
+    # we should never reach this, given the problem specification
     exit(1)
 
 
-def process(scanners):
+def process(scanners: list[Scanner]) -> tuple[list[Scanner], int]:
     rotations = make_rotation_functions()
     # scanner 0 is the reference scanner, first find a match with this one
+    # q will contain matched scanners, with beacon coordinates transformed to coordinate system of scanner 0.
     q = [scanners[0]]
     scanner_positions = []
     del scanners[0]
     while scanners:
         match_idx, t_beacons, scanner_pos = find_next_match(q, scanners, rotations)
         scanner_positions.append(scanner_pos)
-        print("found match")
         del scanners[match_idx]
         q.append(t_beacons)
-    print(
-        "Max distance between scanners:",
-        max(l1(a, b) for a, b in combinations(scanner_positions, 2)),
-    )
-    return q
+    max_dist = max(l1(a, b) for a, b in combinations(scanner_positions, 2))
+    return q, max_dist
 
 
 def main():
     with open("input.txt") as f:
         inp = f.read().splitlines()
     scanners = parse_scanners(inp)
-    res = process(scanners)
+    res, max_dist = process(scanners)
     beacons = set()
     for scanner in res:
         beacons = beacons.union(scanner)
-    print("number of beacons:", len(beacons))
+    print("Max distance between scanners:", max_dist)
+    print("Number of beacons:", len(beacons))
 
 
 if __name__ == "__main__":
